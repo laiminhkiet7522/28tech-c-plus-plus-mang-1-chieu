fb1a23ddac7c554a56d3640042cd0d268884561a
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-05-05 09:32:09 +0700

    In ra so lon nhat va so lon thu hai nang cao

---
+++ In-ra-so-lon-nhat-so-lon-thu-hai-nang-cao/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    do
+    {
+        cout<<"Nhap n la so phan tu mang (n>0): ";
+        cin>>n;
+    }
+    while(n<=0);
+    int M[n];
+    for(int i=0; i<n; i++)
+    {
+        cout<<"Nhap phan tu thu "<<i<<": ";
+        cin>>M[i];
+    }
+    int max1=-1e9-1, max2=-1e9-1;
+    for(int i=0; i<n; i++)
+    {
+        if(M[i]>max1)
+        {
+            max2=max1;
+            max1=M[i];
+        }
+        else if(M[i]>max2 && M[i]!=max1)
+        {
+            max2=M[i];
+        }
+    }
+    if(max2==-1e9-1)
+    {
+        cout<<"So lon nhat trong mang la: "<<max1<<endl;
+        cout<<"So lon thu hai trong mang la: -1 (khong ton tai)"<<endl;
+    }
+    else
+    {
+        cout<<"So lon nhat trong mang la: "<<max1<<endl;
+        cout<<"So lon thu hai trong mang la: "<<max2<<endl;
+    }
+}
